c9a136f52c8dd250169085f3331de684568c9234
Todd Morrison <todd@deepelement.com>
2015-12-07 12:30:18 -0800

    updated build script

--- build/repo-cli.sh
+++ build/repo-cli.sh
@@ -3,27 +3,54 @@
 git clone --quiet https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} travis-build > /dev/null;
 cd travis-build
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
+
+  # Forward Integrate
+  git checkout --track -b integration origin/integration > /dev/null;
+  git merge --squash origin/master --no-commit  > /dev/null;
+  git commit -m "Travis CI auto-merge of master from build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
+  npm version patch -m "Travis CI auto-version from build ${TRAVIS_BUILD_NUMBER}";
+  git push -q origin integration > /dev/null;
+
+  # update local
+  git fetch;
+
+  # Reverse Integrate
+  git checkout master > /dev/null;
+  git merge --squash origin/integration --no-commit  > /dev/null;
+  git commit -m "Travis CI auto-merge of integration from build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
+  git push -q origin master > /dev/null;
+
+fi
+
+if [ "${TRAVIS_BRANCH}" = "stable" ]; then
+
+  # Create tag on staging branch
+  git checkout --track -b stable origin/stable > /dev/null;
+  PACKAGE_VERSION=$(cat package.json \
+      | grep version \
+      | head -1 \
+      | awk -F: '{ print $2 }' \
+      | sed 's/[",]//g')
+  # Trim spaces leading
+  PACKAGE_VERSION=`echo $PACKAGE_VERSION`
+
+  if [ "$PACKAGE_VERSION" != "" ]; then
+      git tag -a "v$PACKAGE_VERSION" -m "`git log -1 --format=%s`"
+      echo "Created a new tag, v$PACKAGE_VERSION"
+  fi
+  git push origin --tags > /dev/null;
+
+  # Reverse Integrate
+  git checkout --track -b integration origin/integration > /dev/null;
+  git merge --squash origin/stable --no-commit  > /dev/null;
+  git commit -m "Travis CI auto-merge of stable from build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
+  git push -q origin integration > /dev/null;
+
   # Reverse Integrate
   git checkout master > /dev/null;
   git merge --squash origin/stable --no-commit  > /dev/null;
-  git commit -m "Travis CI auto-merge from travis build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
+  git commit -m "Travis CI auto-merge of stable from build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
   git push -q origin master > /dev/null;
 
-  # Forward Integrate
-  git checkout --track -b stable origin/stable > /dev/null;
-  git merge origin/master --no-commit  > /dev/null;
-  git commit -m "Travis CI auto-merge from travis build ${TRAVIS_BUILD_NUMBER}  - [ci skip]";
-  npm version patch -m "Travis CI auto-build from travis build ${TRAVIS_BUILD_NUMBER}";
-
-  git push -q origin stable > /dev/null;
-fi
-if [ "${TRAVIS_BRANCH}" = "stable" ]; then
-  git checkout --track -b stable origin/stable > /dev/null;
-  version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
-  if [ "$version" != "" ]; then
-      git tag -a "v$version" -m "`git log -1 --format=%s`"
-      echo "Created a new tag, v$version"
-  fi
-  git push origin --tags > /dev/null;
 fi
 cd ..;
